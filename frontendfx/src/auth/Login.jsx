805b71c4d846cd3e861faf12f817c62db83d88cf
jamadrac <rabbikindalo001@gmail.com>
2025-06-13 15:12:17 +0200

    dd

--- frontendfx/src/auth/Login.jsx
+++ frontendfx/src/auth/Login.jsx
@@ -70,7 +70,7 @@ export const Login = () => {
         setError('Error: ' + err.message);
       }
       setAuthStatus(false);
-    } finally {
+    } finally { 
       setLoading(false);
     }
   };
