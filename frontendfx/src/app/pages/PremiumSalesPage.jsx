a7e6e0c42e170792fcff70862d3ebf9a7c2330cb
jamadrac <rabbikindalo001@gmail.com>
2025-10-22 05:25:24 +0300

    sd

--- frontendfx/src/app/pages/PremiumSalesPage.jsx
+++ frontendfx/src/app/pages/PremiumSalesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import PaymentPopup from "../components/payments/PaymentPopup";
 
 const PremiumSalesPage = () => {
